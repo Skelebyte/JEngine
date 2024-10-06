e4e41ac91efe742d6c0107ffa16f2f13f63f5383
Skelebyte <agsurfer2@gmail.com>
2024-10-06 22:31:38 +0800

    start of big gameloop changes

--- src/JEngine/JEngine.java
+++ src/JEngine/JEngine.java
@@ -18,6 +18,8 @@ public class JEngine {
     static int frames;
     static float frameRate;
 
+
+
     public static void run(Application app) {
         System.out.println("JEngine is functional...");
 
@@ -27,52 +29,33 @@ public class JEngine {
         applicationInstance = app;
 
         applicationInstance.start();
-        float currentTick = System.currentTimeMillis();
-        float currentFrameTime;
 
-        float totalFrameTime = 0.0f;
+        float firstTick = 0.0f;
+        float lastTick = (float) System.nanoTime() / 1000000000;
+        float deltaTime = 0;
+        float unprocessedDeltaTime = 0;
 
-        float frameDelay;
-        //TODO: refactor the frame rate stuff to be in Time class
         while(Window.window.isShowing()) {
 
-            currentFrameTime = System.currentTimeMillis();
+            firstTick = (float) System.nanoTime() / 1000000000;
+
+            deltaTime = firstTick - lastTick;
+
+            lastTick = firstTick;
+
+            unprocessedDeltaTime += deltaTime;
 
             applicationInstance.update();
             Window.setWindowDimensions(new Vector2(Window.window.getSize().width, Window.window.getSize().height));
 
-            Time.setDeltaTime(currentTick, currentFrameTime);
-
-
-            currentTick = currentFrameTime;
-
-            totalFrameTime += Time.deltaTime();
-
-            frames++;
-
-            if(frames >= 100) {
-                frameRate = Mathf.round(frames / totalFrameTime, 2);
-                frames = 0;
-                totalFrameTime = 0.0f;
-
-            }
-
-            frameDelay = Mathf.round(frameRate / 1000, 2);
 
             Window.setMinimumWindowDimensions();
-
-            Time.waitForMilliseconds(frameDelay);
-
 
 
         }
 
         Debug.print("not while");
 
-    }
-
-    public static float getFrameRate() {
-        return frameRate;
     }
 
     static void CreateLogDirectory() {
--- src/JEngine/Time.java
+++ src/JEngine/Time.java
@@ -3,61 +3,6 @@ package JEngine;
 import java.util.concurrent.TimeUnit;
 
 public class Time {
-    static float deltaTime;
-
-    static float startTicks;
-    static float currentTicks;
-
-    public static void setStartTicks() {
-        startTicks = (float) System.currentTimeMillis();
-    }
-
-    public static void setStartTicks(float tick) {
-        startTicks = tick;
-    }
-
-    public static float getStartTicks() {
-        return startTicks;
-    }
-
-    public static void setCurrentTicks() {
-        currentTicks = (float) System.currentTimeMillis();
-    }
-    public static void setCurrentTicks(float value) {
-        currentTicks = Mathf.round(value, 2);
-    }
-
-    public static float getCurrentTicks() {
-        return currentTicks;
-    }
-
-    public static void setDeltaTime() {
-        deltaTime = (currentTicks - startTicks); // / 1000000000;
-
-        if(deltaTime < 0) {
-            deltaTime = (deltaTime - (deltaTime * 2)); // / 1000000000;
-        }
-
-        if (deltaTime < 0.001f) {
-            deltaTime = 0.001f;
-        }
-    }
-
-    public static void setDeltaTime(float a, float b) {
-        deltaTime = (b - a); // / 1000000000;
-
-        if(deltaTime < 0) {
-            deltaTime = (deltaTime - (deltaTime * 2)); // / 1000000000;
-        }
-
-        if (deltaTime < 0.01f) {
-            deltaTime = 0.01f;
-        }
-    }
-
-    public static float deltaTime() {
-        return  deltaTime;
-    }
 
     public static void waitForSeconds(float time) {
         try {
